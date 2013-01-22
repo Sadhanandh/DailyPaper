d08bc7be4641c4148fafb1f41a69c70304c0053f
Sadhanandh Vishwanath <sathanandh@gmail.com>
2013-01-22 13:06:19 +0530

    Updated other cities

--- htepaper.sh
+++ htepaper.sh
@@ -7,11 +7,28 @@
 #
 #		This checks your environment variable $HTE to save the pdf
 #		To Download Any other edition other than Delhi's change the city
+#		
+#		Other Editions..
+#
+#		 Delhi
+#		 Chandigarh
+#		 Bhopal
+#		 Jaipur
+#		 Indore
+#		 Lucknow
+#		 Patna
+#		 Ranchi
+#		 Kolkata
+#		 Mumbai
 #
 #
 #		sathanandh[@]gmail[.]com
 #
 #
+
+CITY="Delhi"
+
+#Change the city to one of the above valid options
 
 if [ -n "$HTE" ]; 
 then  
@@ -27,7 +44,7 @@ if [ ! -f "$location/$today.pdf" ]; then
 	day=$(date +%m | xargs printf "%0d")
 	month=$(date +%d | xargs printf "%0d")
 	year=$(date +%Y)
-	init=$(curl "http://paper.hindustantimes.com/epaper/homepage_v2.aspx?date=$day.$month.$year&width=1366"|grep -E '.*issue.*\(Delhi\)')
+	init=$(curl "http://paper.hindustantimes.com/epaper/homepage_v2.aspx?date=$day.$month.$year&width=1366"|grep -E ".*issue.*\(${CITY}\)")
 	number=$(expr "$init" : ".*issue=\([^']*\)")
 	response=$(curl "http://paper.hindustantimes.com/epaper/PageViewManager.aspx?action=download&issue=$number&page=1&page2=1&cpage=1&cpage2=1")
 	link=$(expr "$response" : '.*href="\([^"]*\)" target')
--- ieepaper.sh
+++ ieepaper.sh
@@ -7,10 +7,23 @@
 #
 #		This checks your environment variable $IEE to save the pdf
 #		To Download Any other edition other than Delhi's change the city
+#			
+#		Other Cities ...
 #
+#		 Delhi
+#		 Mumbai
+#		 Chandigarh
+#		 Pune
+#		 Ahmedabad
+#		 Kolkata
+#		 Lucknow
 #
 #		sathanandh[@]gmail[.]com
 #
+#
+CITY="Delhi"
+#
+#	change the city to any of the above valid option
 #
 if [ -n "$IEE" ]; 
 then  
@@ -27,14 +40,22 @@ if [ ! -d "$location/$today" ]; then
 	mkdir $today
 	cd $today
 
-	r=$(curl -L "http://epaper.indianexpress.com/t/226/latest/Indian-Express")
+if [ "$CITY" == "Delhi" ]; 
+then 
+
+	orig="http://epaper.indianexpress.com/t/226/latest/Indian-Express"
+else
+	paper=$(curl "http://epaper.indianexpress.com/" | grep -E ".*http.*latest.*${CITY}")
+	orig=$(expr "$paper" : '.*href="\([^"]*\)')
+
+fi
+	r=$(curl -L $orig)
 	id=$(expr "$r" : ".*'volumeId': \([^,]*\).*")
 	pages=$(expr "$r" : ".*'numPages': \([^,]*\).*")
 
 	for (( i=1;i <= $pages; i++ ))
 	do
-		wget "http://epaper.indianexpress.com/pdf/get/$id/$i"
-		mv $i $i.pdf
+		wget -O $i.pdf "http://epaper.indianexpress.com/pdf/get/$id/$i"
 	done
 	args=$(ls * |sort -n|xargs echo)
 	pdftk $args cat output $today.pdf
